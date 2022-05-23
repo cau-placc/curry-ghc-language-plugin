a264b5942ee7fb2fd11804f6cd17a0f7d2bdbca5
Kai Prott <kai.prott@hotmail.de>
2022-05-23 11:08:34 +0200

    Add benchmark for sharing across ND

--- sandbox/src/Main.hs
+++ sandbox/src/Main.hs
@@ -18,6 +18,7 @@ import qualified Select
 import qualified SortPrimes
 import qualified TakInt
 import qualified TakPeano
+import qualified SharingAcrossND
 
 import qualified Example
 import qualified Example2
@@ -35,32 +36,35 @@ main = do
   args <- getArgs
   res <- case args of
          [x] -> case x of
-           "AddNum"         -> return $ show $ eval DFS $ AddNum.main
-           "NRev"           -> return $ show $ eval DFS $ NRev.main
-           "PermSort"       -> return $ show $ eval DFS $ PermSort.main
-           "PermSortPeano"  -> return $ show $ eval DFS $ PermSortPeano.main
-           "PrimesHO"       -> return $ show $ eval DFS $ PrimesHO.main
-           "Queens"         -> return $ show $ eval DFS $ Queens.main
-           "ReverseHO"      -> return $ show $ eval DFS $ ReverseHO.main
-           "Select"         -> return $ show $ eval DFS $ Select.main
-           "SortPrimes"     -> return $ show $ eval DFS $ SortPrimes.main
-           "TakInt"         -> return $ show $ eval DFS $ TakInt.main
-           "TakPeano"       -> return $ show $ eval DFS $ TakPeano.main
+           "AddNum"         -> return $ show $ eval DFS  AddNum.main
+           "NRev"           -> return $ show $ eval DFS  NRev.main
+           "PermSort"       -> return $ show $ eval DFS  PermSort.main
+           "PermSortPeano"  -> return $ show $ eval DFS  PermSortPeano.main
+           "PrimesHO"       -> return $ show $ eval DFS  PrimesHO.main
+           "Queens"         -> return $ show $ eval DFS  Queens.main
+           "ReverseHO"      -> return $ show $ eval DFS  ReverseHO.main
+           "Select"         -> return $ show $ eval DFS  Select.main
+           "SortPrimes"     -> return $ show $ eval DFS  SortPrimes.main
+           "TakInt"         -> return $ show $ eval DFS  TakInt.main
+           "TakPeano"       -> return $ show $ eval DFS  TakPeano.main
 
-           "AddNumD"        -> return $ show $ eval DFS $ AddNum.mainD
-           "NRevD"          -> return $ show $ eval DFS $ NRev.mainD
-           "PermSortD"      -> return $ show $ eval DFS $ PermSort.mainD
-           "PermSortPeanoD" -> return $ show $ eval DFS $ PermSortPeano.mainD
-           "PrimesHOD"      -> return $ show $ eval DFS $ PrimesHO.mainD
-           "QueensD"        -> return $ show $ eval DFS $ Queens.mainD
-           "ReverseHOD"     -> return $ show $ eval DFS $ ReverseHO.mainD
-           "SelectD"        -> return $ show $ eval DFS $ Select.mainD
-           "SortPrimesD"    -> return $ show $ eval DFS $ SortPrimes.mainD
-           "TakIntD"        -> return $ show $ eval DFS $ TakInt.mainD
-           "TakPeanoD"      -> return $ show $ eval DFS $ TakPeano.mainD
+           "AddNumD"        -> return $ show $ eval DFS  AddNum.mainD
+           "NRevD"          -> return $ show $ eval DFS  NRev.mainD
+           "PermSortD"      -> return $ show $ eval DFS  PermSort.mainD
+           "PermSortPeanoD" -> return $ show $ eval DFS  PermSortPeano.mainD
+           "PrimesHOD"      -> return $ show $ eval DFS  PrimesHO.mainD
+           "QueensD"        -> return $ show $ eval DFS  Queens.mainD
+           "ReverseHOD"     -> return $ show $ eval DFS  ReverseHO.mainD
+           "SelectD"        -> return $ show $ eval DFS  Select.mainD
+           "SortPrimesD"    -> return $ show $ eval DFS  SortPrimes.mainD
+           "TakIntD"        -> return $ show $ eval DFS  TakInt.mainD
+           "TakPeanoD"      -> return $ show $ eval DFS  TakPeano.mainD
 
            "QueensHs"       -> return $ show [QueensD.main]
            "id"             -> return $ show $ eval1 DFS Example.id True
+           "YesSharingND"   -> return $ show $ eval DFS SharingAcrossND.testSharing
+           "NoSharingND"    -> return $ show $ eval DFS SharingAcrossND.testNoSharing
+           "mainOld"        -> mainOLD >> return ""
            _                -> fail $ "unexpected argument: " ++ x
          _ -> fail $ "expected 1 argument, but got: " ++ show (length args)
   putStrLn res
